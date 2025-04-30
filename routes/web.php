5fd570caa21b338c72483f7c3129b9c298836307
Rafael <RafaelEbenHart_2327240120@mhs.mdp.ac.id>
2025-04-30 18:43:31 +0700

    update laravel

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+use App\Http\Controllers\ProdiController;//jangan lupa import
+use App\Http\Controllers\FakultasController;//jangan lupa import
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+route::get('/profil',function (){
+    return view('profil');
+});
+
+route::resource('/fakultas',FakultasController::class);
+route::resource('/prodi',ProdiController::class);
