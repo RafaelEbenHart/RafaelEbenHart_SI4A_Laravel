cad7fb5ef548239233ba8d743d24d33dbcc1e76f
Rafael <RafaelEbenHart_2327240120@mhs.mdp.ac.id>
2025-07-07 10:54:55 +0700

    update

--- database/migrations/2025_05_31_085149_create_jadwals_table.php
+++ database/migrations/2025_05_31_085149_create_jadwals_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->enum('kode_smt', allowed: ['Gasal', 'Genap']);
             $table->string('kelas', 10);
             $table->foreignId('sesi_id')->constrained('sesi')->onDelete('restrict')->onUpdate('restrict');
-            $table->foreignId('mata_kuliah_id')->constrained('mata_kuliah')->onDelete('restrict')->onUpdate('restrict');
+            $table->foreignId('matakuliah_id')->constrained('matakuliah')->onDelete('restrict')->onUpdate('restrict');
             $table->foreignId('dosen_id')->constrained('users')->onDelete('restrict')->onUpdate('restrict'); // Menggunakan 'users' sebagai tabel dosen
             $table->timestamps();
         });
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('jadwals');
+        Schema::dropIfExists('jadwal');
     }
 };
